ca4f78a3f64681f3ddad5306748e979e0c7d840b
Hakanlsk <hakanlsk20@gmail.com>
2023-11-06 17:25:22 +0300

    first commit

---
+++ src/components/FavouriteList.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import OutfitCategory from "./OutfitCategory/OutfitCategory";
+// import {favoriteIds} from "./OutfitCategory/OutfitCategory";
+
+// function FavouriteList({ favoriteIds }) {
+//     return (
+//       <div>
+//         <h2>Favori Ürünler</h2>
+//         <ul>
+//           {favoriteIds.map((id) => (
+//             <li key={id}>Ürün ID: {id}</li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+
+// export default FavouriteList;
